b99c8494f1aac7e5350e1c1a85bd43f5d2ea5f6c
Bruno Sačarić <bruno.sacaric@gmail.com>
2022-04-14 21:49:23 +0200

    day28

---
+++ day28/pomodoro_timer.py
@@ -0,0 +1,91 @@
+"""
+Pomodoro method app for productivity
+- Work 25 min, break 5 min x3
+- Work 25 min, break 20 min
+"""
+
+import math
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 1
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    global reps, timer
+    reps = 0
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    progress_label.config(text="")
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 2 == 1:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+    elif reps == 8:
+        count_down(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
+    else:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# Gui programs are event driven - they loop through and check for user input constantly
+def count_down(count):
+
+    mins = count // 60
+    seconds = count % 60
+    txt = "%02d:%02d" % (mins, seconds)
+
+    canvas.itemconfig(timer_text, text=txt)
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        for _ in range(math.floor(reps/2)):
+            marks += "✓"
+        progress_label.config(text=marks)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+timer_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=tomato_img)
+timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", command=reset_timer)
+reset_button.grid(column=2, row=2)
+
+progress_label = Label(fg=GREEN, bg=YELLOW)
+progress_label.grid(column=1, row=3)
+
+window.mainloop()
